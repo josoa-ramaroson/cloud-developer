ca13e661e087c2e423cf6232f912eeeef6058644
Josoa RAMAROSON <108058896+josoa-ramaroson@users.noreply.github.com>
2022-09-13 09:58:44 +0300

    Update server.ts

--- course-02/project/image-filter-starter-code/src/server.ts
+++ course-02/project/image-filter-starter-code/src/server.ts
@@ -29,6 +29,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
+  
+   app.get( "/filteredimage/", async ( req, res ) => {
+   let {image_url} = req.query;
+  
+  
+    if ( !image_url ) {
+      return res.status(400)
+                .send(`url is required`);
+    }
+      const reponse = await filterImageFromURL(image_url);
+      res.status(200).sendFile(reponse,()=>{
+        deleteLocalFiles([reponse]);
+      })
+
+  } );
+  
   //! END @TODO1
   
   // Root Endpoint
